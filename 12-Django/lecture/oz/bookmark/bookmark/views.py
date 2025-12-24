c30d043f81e0474c2d22f484bb19e1b2d9a5bee5
dotory-60 <busdotory@gmail.com>
2025-12-24 14:21:30 +0900

    lecture: chapter_02 강의

--- 12-Django/lecture/oz/bookmark/bookmark/views.py
+++ 12-Django/lecture/oz/bookmark/bookmark/views.py
@@ -4,7 +4,7 @@ from bookmark.models import Bookmark
 
 
 def bookmark_list(request):
-    bookmarks = Bookmark.objects.all() # SELECT * FROM bookmark;
+    bookmarks = Bookmark.objects.filter(id__gte=50) # SELECT * FROM bookmark;
     context = { 'bookmarks': bookmarks }
     return render(request, 'bookmark_list.html', context)
 
@@ -12,3 +12,5 @@ def bookmark_detail(request, pk):
     bookmark = get_object_or_404(Bookmark, pk=pk) # SELECT * FROM bookmark WHERE id = pk; 실패하면 404
     context = { 'bookmark': bookmark }
     return render(request, 'bookmark_detail.html', context)
+
+
